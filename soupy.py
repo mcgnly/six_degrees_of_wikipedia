464260204cf6439177c18ba1c831a5ad2f1f1bf5
unknown <03mcginley33@DEBN-L339.msanet.com>
2015-06-19 14:53:18 +0200

    create simple test tree to get a better idea of how the spider is working

---
+++ soupy.py
@@ -0,0 +1,40 @@
+__author__ = '03mcginley33'
+
+page1._url = "Start url"
+page1._links = ["a","b", "c"]
+
+page21._url ="a"
+page21._links =["d", "e", "f"]
+
+page22._url ="b"
+page22._links =["g", "h", "i"]
+
+page23._url ="c"
+page23._links =["j", "k", "l"]
+
+page31._url ="d"
+page31._links =["m", "n", "o"]
+
+page32._url ="e"
+page32._links =["p", "q", "r"]
+
+page33._url ="f"
+page33._links =["s", "t", "u"]
+
+page34._url ="g"
+page34._links =["v", "w", "x"]
+
+page35._url ="h"
+page35._links =["y", "z", "Aa"]
+
+page36._url ="i"
+page36._links =["Bb", "Cc", "Dd"]
+
+page37._url ="j"
+page37._links =["Ee", "Ff", "Gg"]
+
+page38._url ="k"
+page38._links =["Hh", "Ii", "Jj"]
+
+page39._url ="l"
+page39._links =["Kk", "Ll", "Mm"]
